df246c4e55280df9596c95b39d4b986ace48d82a
Lekszikov Miklós <lekszikov.guru@gmail.com>
2022-07-03 11:17:45 +0200

    v2 bug-fix

--- Build/category_modul_torrent.py
+++ Build/category_modul_torrent.py
@@ -12,7 +12,7 @@
 #	[ ]limit
 #		[ ]download
 #		[ ]upload
-#	[ ]list
+#	[X]list
 #	[ ]move
 #	[ ]options
 #	[ ]pause
@@ -33,9 +33,14 @@
 #	[ ]share
 #
 
+from multiprocessing import connection
 import mag
+from mag import *
+import os
+
 
 class torrent_var():
+    global connections
     def torrent_add():
         mag.modul.backa()
 
@@ -59,7 +64,100 @@ class torrent_var():
 
     def torrent_list():
         print("Torrent List")
+        mag.modul.back()
+
+        with open('C://temp//auth_log.txt') as f:
+            connections = f.read()
+        #qbt torrent list [options]
+        
+        #Options
+        #Option	Description
+        #--verbose	Displays verbose information.
+        #--filter <STATUS> -f <STATUS>	Filter by status: all | downloading | seeding | completed | paused | resumed | active | inactive | errored | stalled | stalledDownloading | stalledUploading
+        #--category <CATEGORY> -c <CATEGORY>	Filter by category.
+        #--sort <PROPERTY> -s <PROPERTY>	Sort by property.
+        #--reverse  -r	Reverse the sort order.
+        #--limit <LIMIT> -l <LIMIT>	Number of torrents to display.
+        #--offset <OFFSET> -o <OFFSET>	Offset from the beginning of the list.
+        #--format <LIST_FORMAT> -F <LIST_FORMAT>	Output format: table | list | csv | json
+        #--url <SERVER_URL>	QBittorrent Server URL
+        #--username <USERNAME>	User name
+        #--password <PASSWORD>	User password
+        #--ask-for-password	Ask the user to enter a password in a secure way.
+        #--help -h -?	Show help information
+
+       
+        #Filter by status: all | downloading | seeding | completed | paused | resumed | active | inactive | errored | stalled | stalledDownloading | stalledUploading
+        torrent_list_filter_var = ["all", "downloading", "seeding", "completed", "paused", "resumed", "active", "inactive", "errored", "stalled", "stalledDownloading", "stalledUploading"]
+        xa =[
+            "[ 0  ] :all",
+            "[ 1  ] :downloading",
+            "[ 2  ] :seeding",
+            "[ 3  ] :completed",
+            "[ 4  ] :paused",
+            "[ 5  ] :resumed",
+            "[ 6  ] :active",
+            "[ 7  ] :inactive",
+            "[ 8  ] :errored",
+            "[ 9  ] :stalled",
+            "[ 10 ] :stalledDownloading",
+            "[ 11 ] :stalledUploading"
+            ]
+        print(*xa, sep = "\n" )
+        print("\n")
+        torrent_list_filter_varr = torrent_list_filter_var[int(input("Enter a Number: "))]
+        torrent_listfilter = (" --filter " + torrent_list_filter_varr)
+
+        torrent_list_format_var = ["table", "list", "csv", "json"]
+
+        a =[
+            "[ 0  ] :table",
+            "[ 1  ] :list",
+            "[ 2  ] :csv",
+            "[ 3  ] :json"
+            ]
+        print(*a, sep = "\n" )
+        print("\n")
+
+        torrent_list_format_varr = torrent_list_format_var[int(input("Enter a Number: "))]
+
+        if torrent_list_format_varr == torrent_list_format_var[0]:
+            torrent_listformat = (" --format " + torrent_list_format_varr)
+            logfile = " > C://temp//torrent_list_table.txt"
+
+            #print("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+            #print(torrent_listfilter + torrent_listformat + logfile)
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections)
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+        
+        if torrent_list_format_varr == torrent_list_format_var[1]:
+            torrent_listformat = (" --format " + torrent_list_format_varr)
+            logfile = " > C://temp//torrent_list_list.txt"
+
+            #print("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+            #print(torrent_listfilter + torrent_listformat + logfile)
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections )
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+        
+        if torrent_list_format_varr == torrent_list_format_var[2]:
+            torrent_listformat = (" --format " + torrent_list_format_varr)
+            logfile = " > C://temp//torrent_list_csv.csv"
+
+            #print("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections )
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+    
+        if torrent_list_format_varr == torrent_list_format_var[3]:
+            torrent_listformat = (" --format " + torrent_list_format_varr)
+            logfile = " > C://temp//torrent_list_json.json"
+
+            #print("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections )
+            os.system("qbt torrent list " + torrent_listfilter + torrent_listformat + connections + logfile)
+
         mag.modul.backa()
+                
+
 
     def torrent_move():
         mag.modul.backa()
